f7297d2bd975d3bfb80aa11382e6ae7ca7cf8803
Alessio Palmero Aprosio <alessio@apnetwork.it>
2016-09-14 10:28:19 +0200

    Stuff

--- src/main/java/eu/fbk/microneel/Main.java
+++ src/main/java/eu/fbk/microneel/Main.java
@@ -1,12 +1,16 @@
 package eu.fbk.microneel;
 
-import java.nio.file.Path;
-import java.util.List;
-
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.util.CoreMap;
+import eu.fbk.dh.tint.runner.TintPipeline;
+import eu.fbk.utils.core.CommandLine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eu.fbk.utils.core.CommandLine;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class Main {
 
@@ -14,22 +18,40 @@ public class Main {
 
     public static void main(final String... args) {
         try {
-            // Parse command line
-            final CommandLine cmd = CommandLine.parser().withName("microneel")
-                    .withHeader("Performs NERC, EL, and coreference resolution on microposts.")
-                    .withOption("i", "input",
-                            "specifies the input FILE with the enriched posts to process", "FILE",
-                            CommandLine.Type.FILE_EXISTING, true, false, true)
-                    .withLogger(LoggerFactory.getLogger("eu.fbk")).parse(args);
+//            // Parse command line
+//            final CommandLine cmd = CommandLine.parser().withName("microneel")
+//                    .withHeader("Performs NERC, EL, and coreference resolution on microposts.")
+//                    .withOption("i", "input",
+//                            "specifies the input FILE with the enriched posts to process", "FILE",
+//                            CommandLine.Type.FILE_EXISTING, true, false, true)
+//                    .withLogger(LoggerFactory.getLogger("eu.fbk")).parse(args);
+//
+//            // Read options
+//            final Path inputPath = cmd.getOptionValue("i", Path.class);
+//
+//            // Read posts
+//            final List<Post> posts = Post.read(inputPath);
+//            LOGGER.info("Read {} posts from {}", posts.size(), inputPath);
+//            for (int i = 0; i < posts.size(); ++i) {
+//                LOGGER.info("Post #{}:  {}", i, posts.get(i));
+//            }
 
-            // Read options
-            final Path inputPath = cmd.getOptionValue("i", Path.class);
+//            InputStream configStream = Main.class.getClassLoader().getResourceAsStream("/tint.properties");
+            InputStream configStream = new FileInputStream(
+                    "/Users/alessio/Documents/scripts/microneel/src/main/resources/tint.properties");
 
-            // Read posts
-            final List<Post> posts = Post.read(inputPath);
-            LOGGER.info("Read {} posts from {}", posts.size(), inputPath);
-            for (int i = 0; i < posts.size(); ++i) {
-                LOGGER.info("Post #{}:  {}", i, posts.get(i));
+            TintPipeline pipeline = new TintPipeline();
+            pipeline.loadPropertiesFromStream(configStream);
+            pipeline.loadDefaultProperties();
+            pipeline.load();
+
+            Annotation annotation = pipeline.runRaw("I topi non avevano nipoti");
+
+            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+                for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+                    System.out.println(token);
+                }
+
             }
 
         } catch (final Throwable ex) {
