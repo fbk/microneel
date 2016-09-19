18a04874d640bd0c6080d963d38082de7d405048
Francesco Corcoglioniti <francesco.corcoglioniti@gmail.com>
2016-09-19 16:16:23 +0200

    Skeleton of feature extraction class

---
+++ src/main/java/eu/fbk/microneel/link/SmtSupervisedFeatureExtractor.java
@@ -0,0 +1,16 @@
+package eu.fbk.microneel.link;
+
+import java.util.Set;
+
+import eu.fbk.microneel.Post.MentionAnnotation;
+
+public class SmtSupervisedFeatureExtractor {
+
+    public static Set<String> extractFeatures(MentionAnnotation annotation) {
+    
+        
+        // TODO
+        return null;
+    }
+    
+}
